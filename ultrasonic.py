5cb9ba6cd6e7e5693d032fc8bac35c81d0445583
santiago lemus <santiago.lemus@jet.com>
2019-08-15 18:22:52 -0500

    test 02

--- ultrasonic.py
+++ ultrasonic.py
@@ -2,34 +2,38 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 
-TRIG = 23
+TRIG = 23 
 ECHO = 24
 
-print "Distance Measurement In progress"
+print "Distance Measurement In Progress"
 
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
+try:
+    while True:
 
-GPIO.output(TRIG, False)
-print "Waiting for sensor to settle"
-time.sleep(2)
+        GPIO.output(TRIG, False)
+        print "Waiting For Sensor To Settle"
+        time.sleep(2)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO)==0:
-    pulse_start = time.time()
+        while GPIO.input(ECHO)==0:
+          pulse_start = time.time()
 
-while GPIO.input(ECHO)==0:
-    pulse_end = time.time()
+        while GPIO.input(ECHO)==1:
+          pulse_end = time.time()
 
-pulse_duration = pulse_end - pulse_start
+        pulse_duration = pulse_end - pulse_start
 
-distance = pulse_duration * 17150
+        distance = pulse_duration * 17150
 
-distance = round(distance, 2)
+        distance = round(distance, 2)
 
-print "Distance", distance, "cm"
+        print "Distance:",distance,"cm"
 
-GPIO.cleanup()
+except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+    print("Cleaning up!")
+    gpio.cleanup()
