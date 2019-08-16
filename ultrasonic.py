dd1cf0a7734277e67192a334e2374ad3739465cb
santiago lemus <santiago.lemus@jet.com>
2019-08-15 19:08:54 -0500

    0.5 seconds on each measurement

--- ultrasonic.py
+++ ultrasonic.py
@@ -15,7 +15,7 @@ try:
 
         GPIO.output(TRIG, False)
         print "Waiting For Sensor To Settle"
-        time.sleep(2)
+        time.sleep(0.5)
 
         GPIO.output(TRIG, True)
         time.sleep(0.00001)
